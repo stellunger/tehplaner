ca02d0d9f46b9ae1fcc5e3d227e8749f2d65e1f8
stellunger <1@stellunger.ru>
2014-04-07 17:13:46 +0600

    little new2

--- html/mail.php
+++ html/mail.php
@@ -8,7 +8,7 @@ $message = "Привет!\n\n";
 
 if ($_SERVER['REQUEST_METHOD']=='POST') {
     $message .= "Имя: " . $_POST['name'] . "\n";
-    $message .= "Email: " . $_POST['email'] . "\n";
+    //$message .= "Email: " . $_POST['email'] . "\n";
     $message .= "Телефон: " . $_POST['phone'] . "\n";    
     $message .= $_POST['extra'];
 

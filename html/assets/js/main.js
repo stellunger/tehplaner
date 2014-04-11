4e82071cef68334a77d658e6c1c6b8e355c058db
stellunger <1@stellunger.ru>
2014-04-11 13:01:37 +0600

    pasport

--- html/assets/js/main.js
+++ html/assets/js/main.js
@@ -9,3 +9,15 @@ $('#form').on('submit', function(event) {
         $('#form').html('<h1>Спасибо! Мы свяжемся с вами!</h1>');    
     });
 });
+
+$('#form1').on('submit', function(event) {
+    event.preventDefault();
+
+    $.post('/mail1.php', {
+        name: $('#name').val(),
+        email: $('#email').val(),
+        phone: $('#phone').val(),        
+    }, function(){
+        $('#form1').html('<h1>Спасибо! Мы свяжемся с вами!</h1>');    
+    });
+});
